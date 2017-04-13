b3e15241fefa238c3c0b32212d6ca03de3079433
kinjalchotaliya <kinjal.chotaliya@bacancytechnology.com>
2017-04-13 18:13:11 +0530

    Revert "Authentication Via Facebook"
    This reverts commit c427d3bd93df7eaea07acf698b83b47a1dd1ffbf.

--- Facebook_auth/config/routes.rb
+++
@@ -1,7 +0,0 @@
-Rails.application.routes.draw do
-  get 'home/index'
-
-  devise_for :users,:controllers =>{ :omniauth_callbacks => "omniauth_callbacks" }
-root 'home#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
